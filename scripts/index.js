d1ca4a075babbfb89969435bf9b755edd4b9372f
Flavio Silva <fal_silva@hotmail.com>
2023-12-10 00:21:38 -0300

    alteração de frase no JS

--- scripts/index.js
+++ scripts/index.js
@@ -62,7 +62,7 @@ document.addEventListener('visibilitychange',
 // cria um array de objetos, com as frases a serem escritas,
 // se vai durar sempre, a velocidade da escrita, a velocidade de volta da escrita e o atraso para escrever.
 var typed = new Typed(".escrever-texto", {
-  strings: ["avalio para porte de armas", "realizo seleção e treinamento."],
+  strings: ["avalio para porte de armas", "realizo seleção e treinamento", "faço avaliação de desempenho."],
   loop: true,
   typeSpeed: 50,
   backSpeed: 25,
