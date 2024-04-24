1f5efa8144b33480c93354a0c471a1de910f0520
Flavio Silva <fal_silva@hotmail.com>
2024-04-24 00:00:47 -0300

    criação script slider js

---
+++ scripts/slider.js
@@ -0,0 +1,19 @@
+// Ao clicar nas esferas
+
+let radio = document.querySelectorAll('.manual-btn')
+let cont = 1;
+document.getElementById('radio1').checked = true
+
+
+setInterval(() => {
+  proximaImg()
+}, 5000)
+
+function proximaImg(){
+  cont++
+    if (cont > 3){
+      cont= 1
+  }
+
+  document.getElementById('radio'+cont).checked = true
+}
