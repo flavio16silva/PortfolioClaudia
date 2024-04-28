bca67df13259a6d34c73c981d2572cbcbb289d79
Flavio Silva <fal_silva@hotmail.com>
2024-04-27 22:00:24 -0300

    alteração da medida do scroll para 560

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,5 +1,5 @@
 
 window.addEventListener("scroll", function() {
   let navbar = document.querySelector('.navbar')
-  navbar.classList.toggle('rolagem', window.scrollY > 680);
+  navbar.classList.toggle('rolagem', window.scrollY > 560);
 })
